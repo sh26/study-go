26fe5cb020dbf76bb6970579d86ff29f372a232c
sh26 <sh26@kakao.com>
2023-01-01 23:53:49 +0900

    baekjoon step04 문제 풀이 (1330, 9498)

--- baekjoon/code/step04/q01330.go
+++ baekjoon/code/step04/q01330.go
@@ -2,6 +2,29 @@
 
 package step04
 
-func q01330() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func q01330() {
+	r, w := bufio.NewScanner(os.Stdin), bufio.NewWriter(os.Stdout)
+	r.Split(bufio.ScanWords)
+	defer w.Flush()
+
+	scanInt := func() (num int) {
+		r.Scan()
+		num, _ = strconv.Atoi(r.Text())
+		return
+	}
+
+	if a, b := scanInt(), scanInt(); a < b {
+		fmt.Fprint(w, "<")
+	} else if a > b {
+		fmt.Fprint(w, ">")
+	} else {
+		fmt.Fprint(w, "==")
+	}
 }
--- baekjoon/code/step04/q09498.go
+++ baekjoon/code/step04/q09498.go
@@ -2,6 +2,23 @@
 
 package step04
 
-func q09498() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func q09498() {
+	r, w := bufio.NewScanner(os.Stdin), bufio.NewWriter(os.Stdout)
+	r.Split(bufio.ScanWords)
+	defer w.Flush()
+
+	scanInt := func() (num int) {
+		r.Scan()
+		num, _ = strconv.Atoi(r.Text())
+		return
+	}
+
+	fmt.Fprint(w, string("FFFFFFDCBAA"[scanInt()/10]))
 }
