e5cab8d123a3de73098ac3abef915b5e49bb33da
sh26 <sh26@kakao.com>
2023-01-09 17:46:54 +0900

    programmers level1 주석 오타 수정

--- programmers/code/level1/code.go
+++ programmers/code/level1/code.go
@@ -1,4 +1,4 @@
-// https://https://school.programmers.co.kr/learn/challenges?levels=1&languages=go
+// https://school.programmers.co.kr/learn/challenges?levels=1&languages=go
 package level1
 
 // Answer is programmers/code/level1 main function
