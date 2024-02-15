150c05996d3432eb58a42d97e90801a34a5f83f5
sh26 <sh26@kakao.com>
2024-02-15 11:52:37 +0900

    baekjoon 공 넣기

---
+++ baekjoon/code/q10810.go
@@ -0,0 +1,40 @@
+package code
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+// q10810 is a function to solve the problem "공 넣기".
+// source: https://www.acmicpc.net/problem/10810
+func q10810() {
+	r, w := bufio.NewScanner(os.Stdin), bufio.NewWriter(os.Stdout)
+	r.Split(bufio.ScanWords)
+	r.Scan()
+	N, _ := strconv.Atoi(r.Text())
+	r.Scan()
+	M, _ := strconv.Atoi(r.Text())
+
+	baskets := make([]int, N)
+	for i := 0; i < M; i++ {
+		r.Scan()
+		start, _ := strconv.Atoi(r.Text())
+		r.Scan()
+		end, _ := strconv.Atoi(r.Text())
+		r.Scan()
+		num, _ := strconv.Atoi(r.Text())
+		for j := start - 1; j <= end-1; j++ {
+			baskets[j] = num
+		}
+	}
+
+	for i, num := range baskets {
+		if i != 0 {
+			fmt.Fprint(w, " ")
+		}
+		fmt.Fprint(w, num)
+	}
+	w.Flush()
+}
